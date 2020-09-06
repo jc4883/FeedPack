4f7e921f37cdd21920faeec88cca1710ca479951
jc4883 <jc4883@columbia.edu>
2020-09-05 20:14:38 -0700

    Add Digital Ocean ubuntu ip address to settings.py

--- django_react/settings.py
+++ django_react/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['165.227.92.55']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
