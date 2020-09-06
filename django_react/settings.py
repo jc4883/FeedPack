0c55770c0b83d4dc7c9313a1486af0ffbba6d296
jc4883 <jc4883@columbia.edu>
2020-09-05 20:30:42 -0700

    Update settings.py to use separate database configuration on DEBUG=FALSE

--- django_react/settings.py
+++ django_react/settings.py
@@ -14,6 +14,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import environ
+import os
 
 env = environ.Env(
     # set casting, default value
@@ -96,10 +97,21 @@ WSGI_APPLICATION = 'django_react.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    "default": env.db()
-}
+if DEBUG:
+    DATABASES = {
+        "default": env.db()
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'myproject',
+            'USER': 'admin',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
@@ -139,3 +151,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
